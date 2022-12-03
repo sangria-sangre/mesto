c69345f2d7ba0422646eaff3633530467b7b49c9
Romanova Alexandra <miss.sandray@yandex.ru>
2022-12-03 22:46:13 +0300

    Создание веб страницы в соответствие с представленным макетом

---
+++ index.js
@@ -0,0 +1,35 @@
+let btnEdit = document.querySelector('.profile__btnEdit');
+let btnClose = document.querySelector('.popup__btnClose');
+let btnSave = document.querySelector('.popup__btnSave');
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+let inputTitle = document.querySelector('#title');
+let inputSubtitle = document.querySelector('#subtitle');
+
+inputTitle.value = title.textContent;
+inputSubtitle.value = subtitle.textContent;
+
+
+function openPopup () {
+    let popup = document.querySelector('.popup');
+    popup.classList.remove('popup');
+    popup.classList.add('popup_active');
+}
+
+function closePopup () {
+    let popup = document.querySelector('.popup_active');
+    popup.classList.remove('popup_active');
+    popup.classList.add('popup');
+    inputTitle.value = title.textContent;
+    inputSubtitle.value = subtitle.textContent;
+}
+
+function savePopup () {
+    title.textContent = inputTitle.value;
+    subtitle.textContent = inputSubtitle.value;
+    closePopup (); 
+}
+
+btnEdit.addEventListener('click', openPopup);
+btnClose.addEventListener('click', closePopup);
+btnSave.addEventListener('click', savePopup);
